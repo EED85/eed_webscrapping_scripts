a5d2db0c8fbacdbcf9be5d564111583057b4e8b1
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2025-02-23 14:25:09 +0100

    passt (#4)
    ES-252 #close

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import duckdb
+
+
+def download_json_to_duckdb(url, con):
+    con.sql(f"""
+        CREATE OR REPLACE TABLE data AS FROM read_json('{url}');
+    """)
+
 def main():
+    url = 'https://opendata.dwd.de/climate_environment/health/alerts/s31fg.json'
+    con = duckdb.connect('dwd.duckdb')
     print("Hello from eed-webscrapping-scripts!")
+    download_json_to_duckdb(url, con)
 
 
 if __name__ == "__main__":
