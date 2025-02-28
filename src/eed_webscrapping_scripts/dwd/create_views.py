cb5cefcc05aa58bec2989351dff1a05a3a7e7f79
EED85 <eric.brahmann@eed-solutions.de>
2025-02-28 06:33:46 +0100

    create view für Datalake hat funktioniert

---
+++ src/eed_webscrapping_scripts/dwd/create_views.py
@@ -0,0 +1,20 @@
+from eed_webscrapping_scripts.dwd import get_config, prepare_db
+from eed_webscrapping_scripts.modules import read_sql_file
+
+
+def create_views():
+    cfg = get_config()
+    datalake = cfg["pollenflug_gefahrenindex"]["db_infos"]["datalake"]
+    sql = read_sql_file("dwd\sqls\list_available_tables.sql", cfg["git_root"])
+    con = prepare_db()
+    _tables = con.sql(sql).fetchall()
+    tables = [item[0] for item in _tables]
+    sql_union_view_base = " UNION BY NAME ".join(
+        [f"FROM {datalake}.{table}" for table in tables]
+    )
+    sql_union_view = f"CREATE OR REPLACE VIEW {datalake}.Pollenflug_Gefahrenindex AS ({sql_union_view_base})"
+    con.sql(sql_union_view)
+
+
+if __name__ == "__main__":
+    create_views()
--- src/eed_webscrapping_scripts/dwd/db.py
+++ src/eed_webscrapping_scripts/dwd/db.py
@@ -1,17 +1,8 @@
-import os
-import duckdb
-
-home_dir = os.path.expanduser("~")
+from eed_webscrapping_scripts.modules import connect_to_db
 
 
-def prepare_db():
-    try:
-        with open(os.path.join(home_dir, ".motherduck_token")) as f:
-            md_token = f.read()
-    except Exception:
-        md_token = os.getenv("MD_TOKEN")
-
-    con = duckdb.connect(f"md:?motherduck_token={md_token.strip()}")
+def prepare_db(con=None):
+    con = con or connect_to_db()
     con.sql("CREATE DATABASE IF NOT EXISTS dwd")
     con.sql("USE dwd")
     con.sql("CREATE SCHEMA IF NOT EXISTS datalake")
