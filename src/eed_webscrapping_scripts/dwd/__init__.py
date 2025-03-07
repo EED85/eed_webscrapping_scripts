784050f635ab58ac0637c24761c6cdce81e63225
EED85 <eric.brahmann@eed-solutions.de>
2025-03-07 09:04:30 +0100

    main.py klappt auch

--- src/eed_webscrapping_scripts/dwd/__init__.py
+++ src/eed_webscrapping_scripts/dwd/__init__.py
@@ -3,9 +3,6 @@ __all__ = [
     "download_json_to_duckdb",
     "get_config",
     "read_sql_file",
-    # "pollenflug_gefahrenindex",
 ]
 from .db import prepare_db
-
-# from .pollenflug_gefahrenindex import pollenflug_gefahrenindex
 from .utils import download_json_to_duckdb, get_config
