f27ff95dc83186fcf11eb9c9549b57083ea29972
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2025-03-23 19:27:44 +0100

    Feature/es 275 2503 verschlusselung hinzufugen (#37)
    * add selenium
    
    * add soup
    
    * add requests
    
    * uv add cryptography
    
    * lokale pytests laufen
    
    * add encrition result
    
    * neuer versuch
    
    * lokal klappt es immer noch - neuer versuch
    
    * lokal klappt es
    
    * aufgegeben

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+import os
+from collections.abc import Iterator
 from pathlib import Path
 
 import pytest
 import yaml
+
+from eed_webscrapping_scripts.modules import get_encryption_pasword
 
 
 # Fixture to use the mock_load_config function
@@ -12,3 +16,14 @@ def patch_get_config_dwd(monkeypatch):
             return yaml.safe_load(file)
 
     monkeypatch.setattr("eed_webscrapping_scripts.dwd.get_config", get_config_dwd_mock)
+
+
+@pytest.fixture
+def cfg_test() -> Iterator[dict]:
+    home_dir = os.path.expanduser("~")
+    password = get_encryption_pasword()
+    cfg_test = {
+        "home_dir": home_dir,
+        "encrytpion": {"password": password},
+    }
+    yield cfg_test
