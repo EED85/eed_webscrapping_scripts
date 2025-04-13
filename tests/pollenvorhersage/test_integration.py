0d0ce431eeebc45a55ffc4f34f9db4bfa7df418b
EED85 <eric.brahmann@eed-solutions.de>
2025-04-13 22:16:34 +0200

    class implemented

--- tests/pollenvorhersage/test_integration.py
+++ tests/pollenvorhersage/test_integration.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from eed_webscrapping_scripts.pollenvorhersage.pollenvorhersage import pollenvorhersage
+from eed_webscrapping_scripts.pollenvorhersage.pollenvorhersage import PollenvorhersageHandler
 
 
 def test_pollenvorhersage(cfg_test):
-    con = pollenvorhersage()
+    pollenvorhersage_handler = PollenvorhersageHandler()
+    con = pollenvorhersage_handler.fetch_and_store_html()
     con.sql("SELECT 1 AS ONE")
     con.close()
     Path.unlink("pollenvorhersage.duckdb")
